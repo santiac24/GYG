58a1cfebb72b0d77b1432510ca4c995ad58349c5
valeriapadilla <vpadillahoyos@gmail.com>
2023-10-14 16:00:06 -0500

    Prueba archivo CD_usuario

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -1,15 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
 
 namespace CapaDatos
 {
-    internal class Conexion
+    public class CD_Usuario
     {
-        public static string cadena = ConfigurationManager.ConnectionStrings["cadena_conexion"].ToString();
+        public List<Usuarios> Listar()
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "select Id_usuario, NombreUsuario, Contrasena, Rol_id from usuarios";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuarios()
+                            {
+                                Id_usuario = Convert.ToInt32(dr['Id_usuario']);
+                                NombreUsuario= dr['NombreUsuario'].ToString();
+                                Contrasena =dr['Contrasena'].ToString();
+                                Rol_id = Convert.ToInt32(dr['Rol_id']);
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lista = new List<Usuarios>();
+                }
+            }
+
+            return lista;
+        }
 
     }
 }
