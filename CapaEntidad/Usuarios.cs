2fb24847976ff53eca52ad38e01f8be6fa131dea
Valeria Padilla <81450427+valeriapadilla@users.noreply.github.com>
2023-10-24 16:51:11 -0500

    Update Usuarios.cs

--- CapaEntidad/Usuarios.cs
+++ CapaEntidad/Usuarios.cs
@@ -9,7 +9,7 @@ namespace CapaEntidad
         public string Nombre { get; set; } = "";
         public string Usuario { get; set; } = "";
         public string Contrasena { get; set; } = "";
-        public Roles o_Rol { get; set; };
+        public Roles o_Rol { get; set; }
     }
 }
 
